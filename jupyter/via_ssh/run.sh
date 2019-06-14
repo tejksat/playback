84e55f6cb1b9c4773bd65ee18c369fb71933639d
Alexander Koshevoy <Alexander.Koshevoy@jetbrains.com>
2019-06-14 18:08:48 +0300

    Add optional port parameter to run.sh

--- jupyter/via_ssh/run.sh
+++ jupyter/via_ssh/run.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
+OPTIND=1
+OPT_PORT=2222
+
+while getopts "p:" opt; do
+    case "$opt" in
+    p)  OPT_PORT=$OPTARG
+        ;;
+    esac
+done
+
 IMAGE_TAG=dev
 
-CONTAINER_ID=$(docker run -d --rm --publish 127.0.0.1:2239:22/tcp intellij/jupyter-ssh:$IMAGE_TAG)
+CONTAINER_ID=$(docker run -d --rm --publish 127.0.0.1:$OPT_PORT:22/tcp intellij/jupyter-ssh:$IMAGE_TAG)
 
 echo $CONTAINER_ID > .container_id
 
