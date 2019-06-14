8d598a81f8fbe19d94ab70a0447cbf3ad68603d4
Alexander Koshevoy <Alexander.Koshevoy@jetbrains.com>
2019-06-14 19:54:21 +0300

    Fix the owner of authorized_keys

--- jupyter/via_ssh/run.sh
+++ jupyter/via_ssh/run.sh
@@ -17,3 +17,6 @@ CONTAINER_ID=$(docker run -d --rm --publish 127.0.0.1:$OPT_PORT:22/tcp intellij/
 echo $CONTAINER_ID > .container_id
 
 docker cp ~/.ssh/id_rsa.pub $CONTAINER_ID:/root/.ssh/authorized_keys
+
+# fix the owner of authorized_keys
+docker exec $CONTAINER_ID chown root:root /root/.ssh/authorized_keys
